c9871999fec675ab0e195e2d4e481bdc51e47c34
Harshal Lahoti <harshallahoti95@gmail.com>
2026-07-24 22:28:37 +0530

    fix: InitGoogleTest expects char** not char***

--- tests/smoke_test.cpp
+++ tests/smoke_test.cpp
@@ -26,6 +26,6 @@ TEST(Proto, ChunkPlacementHoldsReplicas) {
 }
 
 int main(int argc, char** argv) {
-  ::testing::InitGoogleTest(&argc, &argv);
+  ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
